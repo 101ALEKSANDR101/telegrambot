1366433d4051756d41057720696bdf2b4b066f91
101ALEKSANDER101 <lx2006861@gmail.com>
2024-01-28 17:39:44 +0300

    add pg full

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize');
 
 
 module.exports = new Sequelize(
-	'bot',
-	'postgres',
-	'alex200686',
+	'telegrambot',
+	'root',
+	'root',
 	{
-		host: 'localhost',
+		host: 'lmaster.13ce5085-5114-487e-8fbd-5c5d5236d8be.c.dbaas.selcloud.ru',
 		port: '5432',
 		dialect: 'postgres'
 	}
